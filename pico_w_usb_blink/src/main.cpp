9170fe575f76add502c32084755017846ebfff40
Edward Görlich <goerlich_e@tfs.pl>
2023-05-18 20:17:48 +0200

    pico w usb blink ok

--- pico_w_usb_blink/src/main.cpp
+++ pico_w_usb_blink/src/main.cpp
@@ -1,21 +1,23 @@
 #include <Arduino.h>
 #include "pico/cyw43_arch.h"
 
+
 void setup() {
-   Serial.begin(115200);
-     if (cyw43_arch_init()) 
-        printf("Wi-Fi init failed");
+  Serial.begin(115200);
+  if (cyw43_arch_init()) 
+    Serial.println("Wi-Fi init failed");
 }
    
 #define CYW43_WL_GPIO_LED_PIN 0
+#define DELAY 1000
 
 void loop() {
-  // Serial.print("Hello World!\r\n"); 
-  // delay(1000);
-  Serial.print("ON\n");
+  //Serial.print("ON\r\n");
+  Serial.println("ON");
   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-  delay(1000);
-  printf("OFF\n");
+  sleep_ms(DELAY);
+  //Serial.print("ON\r\n");
+  Serial.println("OFF");
   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-  sleep_ms(1000);
+  sleep_ms(DELAY);
 }
